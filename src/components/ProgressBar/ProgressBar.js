bdc77ed6540747b280f2bcea9f948f2f291ea9b6
Lauren Beatty <lauren.beatty@treasure-data.com>
2021-05-17 09:00:37 -0500

    good start

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,8 +5,41 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = {
+  small: {
+    "--borderRadius": 4 + "px",
+    "--padding": "0px",
+    "--height": "8px"
+  },
+  medium: {
+    "--borderRadius": 4 + "px",
+    "--padding": "0px",
+    "--height": "12px"
+  },
+  large: {
+    "--borderRadius": 8 + "px",
+    "--padding": "4px",
+    "--height": "16px"
+  }
+};
+
+const Wrapper = styled.div`
+  background: ${COLORS.transparentGray15};
+  border-radius: var(--borderRadius);
+  padding: var(--padding);
+  box-shadow: inset 0px 2px 4px rgba(128, 128, 128, 0.35);
+  width: 100%;
+`;
+
+const InnerBar = styled.div(({value}) => ({
+  width: `${value}%`,
+  background: COLORS.primary,
+  height: `var(--height)`,
+  borderRadius: "4px 0 0 4px"
+}))
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const styles = SIZES[size];
+  return <Wrapper role="progressbar" aria-valuenow={value} aria-valuemin="0" aria-valuemax="100" size={size} style={styles}><InnerBar style={styles} value={value}><VisuallyHidden>{value}</VisuallyHidden></InnerBar></Wrapper>;
 };
 
 export default ProgressBar;
